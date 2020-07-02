9e7b8251fbf12d1f7ec8375ef2cafdb99e6273e5
DenEdwards <denzelsedwards@gmail.com>
2020-07-01 20:07:26 -0400

    Advancing security throuugh basic hashing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -20,8 +20,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
-
 const User = mongoose.model("User", userSchema);
 
 app.get("/", function(req,res){
@@ -34,7 +32,7 @@ app.get("/register", function(req,res){
 
 app.post("/register", function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   const newUser = new User({
     email: username,
@@ -56,7 +54,7 @@ app.get("/login", function(req,res){
 
 app.post("/login", function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({email: username}, function(err, foundResult){
     if(!err){
       if(foundResult){
