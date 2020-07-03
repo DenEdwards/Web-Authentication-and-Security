a0f48343054c19410951413edb967907b0089d12
DenEdwards <denzelsedwards@gmail.com>
2020-07-02 20:20:22 -0400

    Using OAuth and third Party authentication

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -14,40 +18,128 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+//setting up our app to use session with some initial configuration
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+//tell passport to start using it for authentication
+app.use(passport.initialize());
+//tell our app to use passport to setup our session
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+  facebookId: String,
+  secret: String
 });
 
+//Used to hash and salt our passwords as well as store it in our mongo db
+userSchema.plugin(passportLocalMongoose);
+
+userSchema.plugin(findOrCreate);
+
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+//Use passport to serialize and deserialize our user
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+//Using google Strategy to log in our user
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id,username: profile.displayName }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+//Using facebook Strategy to log in our user
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log(profile);
+    User.findOrCreate({ facebookId: profile.id, username: profile.displayName }, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      done(null, user);
+    });
+  }
+));
 
 app.get("/", function(req,res){
   res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
+
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook',
+  passport.authenticate('facebook', { scope: ["email"] })
+);
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/secrets',
+    passport.authenticate('facebook', { successRedirect: '/secrets',
+                                        failureRedirect: '/login' }));
 
 app.get("/register", function(req,res){
   res.render("register");
 });
 
 app.post("/register", function(req,res){
-  const username = req.body.username;
-  const password = req.body.password;
-
-  bcrypt.hash(password, saltRounds, function(err, hash) {
-    const newUser = new User({
-      email: username,
-      password: hash
-    });
-    newUser.save(function(err){
-      if(err){
-        console.log(err);
-      }else{
-        console.log("New User Created!");
-        res.render("secrets");
-      }
-    });
+  //comes from passportLocalMongoose package as a middle man to register and save our user to our mongoose db
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    }else{
+      //callback only triggered if authentication was successful
+      //authenticate user using username and password
+      //send a cookie and tell browser to hold onto that cookie that says the user is authorized to view the secrets page
+      passport.authenticate("local")(req,res, function(){
+        res.redirect("/secrets");
+      });
+    }
   });
 });
 
@@ -56,21 +148,64 @@ app.get("/login", function(req,res){
 });
 
 app.post("/login", function(req,res){
-  const username = req.body.username;
-  const password = req.body.password;
-  User.findOne({email: username}, function(err, foundResult){
-    if(!err){
-      if(foundResult){
-        bcrypt.compare(password, foundResult.password, function(err, result) {
-          if(result){
-            res.render("secrets");
-          }
-        });
-      }else{
-        res.redirect("login");
-      }
-    }else{
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  //From passport
+  req.login(user, function(err){
+    if(err){
       console.log(err);
+    }else{
+      //callback only triggered if authentication was successful
+      //authenticate user using username and password
+      //send a cookie and tell browser to hold onto that cookie that says the user is authorized to view the secrets page
+      passport.authenticate("local")(req,res, function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
+
+app.get("/secrets", function(req,res){
+  User.find({"secret": {$ne: null}}, function(err, resultsFound){
+    if(err){
+      console.log(err);
+    }else{
+      if(resultsFound){
+        res.render("secrets", {userWithSecrets: resultsFound});
+      }
+    }
+  });
+});
+
+app.get("/logout", function(req,res){
+  //passport
+  req.logout();
+  res.redirect("/");
+});
+
+app.get("/submit", function(req,res){
+  if(req.isAuthenticated()){
+    res.render("submit");
+  }else{
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit", function(req,res){
+  const submittedSecret = req.body.secret;
+  console.log(req.user);
+  User.findById(req.user.id, function(err,foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
     }
   });
 });
